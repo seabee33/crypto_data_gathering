397b8c5ec9e2e151e49be2f02403c63cbaa5d4cb
SeaBee <85791528+seabee33@users.noreply.github.com>
2024-09-06 17:32:16 +1000

    Update core.py

--- core.py
+++ core.py
@@ -5,93 +5,119 @@ from m_functions import *
 from m_fred import *
 from m_calculations import *
 from m_cq import *
+from m_art_snowflake import *
 db_name = os.getenv("DB_NAME")
 db_username = os.getenv("DB_USERNAME")
 db_password = os.getenv("LOCAL_DB_PASSWORD")
-counter = 1
 
+# Artemis
+update_art_sf = True
+update_artemis = True
 
-update_artemis = False
+# Token Terminal
 update_token_terminal = True
-update_bitformance = False
-update_fred = False
-update_smas = False
+
+# Bitformance
+update_bitformance = True
+
+# FRED
+update_fred = True
+
+# Crypto Quant
 update_cq = False
+
+# Calculations
+update_smas = True
 update_main_table = True
+
+
 conn = mysql.connector.connect(host="localhost", database=db_name, user=db_username, password=db_password)
+conn_cq = mysql.connector.connect(host="localhost", database="helios-cq", user=db_username, password=db_password)
+conn_sf = snowflake.connector.connect(user='conordb', password=sf_db_pw, account=sf_acc_id, warehouse='COMPUTE_WH', database='ARTEMIS_DATA')
 
 try:
+	new_log_entry(conn, ("g", "Core", f"Beginning updates for {'Artemis (API),' if update_artemis else ''} {'Artemis (SF),' if update_art_sf else ''} {'TT,' if update_token_terminal else ''} {'Bitformance,' if update_bitformance else ''} {'FRED,' if update_fred else ''} {'CQ,' if update_cq else ''}  {'SMAs,' if update_smas else ''}  {'Main Table' if update_main_table else ''}"))
+
+	if update_art_sf:
+		sf_cursor = conn_sf.cursor()
+		sf_cursor.execute("SELECT 1")
+		new_log_entry(conn, ("g", "Core", "Beginning update for SF Artemis"))
+		sf_art_update_raw_data(conn_sf, conn)
+		new_log_entry(conn, ("g", "Core", "finished update for SF Artemis successfully"))
+
 	if conn.is_connected():
 		cursor = conn.cursor()
-		print("Connected to DB ^.^")
-		new_log_entry(conn, ("g", "Core", f"Starting script"))
 		
 		if update_artemis:
 			# ============ ARTEMIS ============
 			# Update all projects list
 			# art_update_all_projects_list(cursor, conn, api_query)
-			# new_log_entry(conn, ("g", "Core", "Step 1/11"))
 
 			# # Get all available metric keys
 			# art_get_api_project_metrics(cursor, conn, api_query)
-			# new_log_entry(conn, ("g", "Core", "Step 2/11"))
 
 			# # Add wanted metrics to db from list
 			# art_add_selected_metrics(cursor, conn)
-			# new_log_entry(conn, ("g", "Core", "Step 3/11"))
 
 			# # Update unique metrics table with every unique metric key
 			# art_update_unique_metrics_table(cursor, conn)
-			# new_log_entry(conn, ("g", "Core", "Step 4/11"))
 
 			# Then update metric data itself
+			new_log_entry(conn, ("g", "Core", "Beginning update for artemis data"))
 			art_update_metric_data(cursor, conn, art_api_key)
-			new_log_entry(conn, ("g", "Core", "Step 5/11"))
+			new_log_entry(conn, ("g", "Core", "Finished update for artemis data successfully"))
 
 			# Optional, update ecosystems (not projects)
 			#art_update_supported_ecosystems_from_api(cursor, conn, api_query)
 
-
 		if update_token_terminal:
+			new_log_entry(conn, ("g", "Core", "Beginning update for token terminal"))
 			# ============ TOKEN TERMINAL ============
 			# Get a list of all market sectors
 			# tt_update_all_market_sectors_list(cursor, conn, tt_api_key)
-			# new_log_entry(conn, ("g", "core", "Step 6/11"))
 
 			# Apply market sector to project list
 			# tt_update_project_ids_with_market_sector(cursor, conn, tt_api_key)
-			# new_log_entry(conn, ("g", "core", "Step 8/11"))
 
 			diff = tt_compare_available_metrics(conn)
 			
 			if diff == []:
 				tt_update_project_list(cursor, conn, tt_api_key)
-				new_log_entry(conn, ("g", "Core", "Step 9/11"))
 				tt_update_project_metrics(cursor, conn, tt_api_key)
-				new_log_entry(conn, ("g", "Core", "Step 10/11"))
+				new_log_entry(conn, ("g", "Core", "finished update for tt successfully"))
 			else:
 				print("Uh Oh, new metric available!")
 				new_log_entry(conn, ("h", "Token Terminal", f"New Metric, Can not continue, difference: {diff}"))
 				print(diff)
 
 		if update_bitformance:
+			new_log_entry(conn, ("g", "Core", "Beginning update for bitformance data"))
 			bf_update_data(conn)
+			new_log_entry(conn, ("g", "Core", "Finished update for bitformance successfully"))
 
 		if update_fred:
+			new_log_entry(conn, ("g", "Core", "Beginning update for fred data"))
 			fred_update_data(conn)
+			new_log_entry(conn, ("g", "Core", "Finished update for fred successfully"))
 		
 		if update_smas:
+			new_log_entry(conn, ("g", "Core", "Beginning update for SMA"))
 			calculate_sma(conn)
+			new_log_entry(conn, ("g", "Core", "Finished update for SMA"))
 		
 		if update_main_table:
+			new_log_entry(conn, ("g", "Core", "Beginning update for main table data"))
 			calc_update_raw_table(conn)
-		
+			new_log_entry(conn, ("g", "Core", "Finished update for main table successfully"))
+	
 		if update_cq:
+			new_log_entry(conn, ("g", "Core", "Beginning update for cq data"))
 			# Update exchange list
 			# cq_update_exchange_data(conn)
 
 			# ===== Update Exchange Flows ===== 
-			cq_update_test(conn)
+			cq_update(conn_cq)
+			new_log_entry(conn, ("g", "Core", "Finished update for cq successfully"))
 
 	else:
 		print("Conn not connected")
@@ -101,15 +127,10 @@ except Error as e:
 	new_log_entry(conn, ("h", "Core", f"Error: {e}"))
 
 finally:
-	# if conn.is_connected():
-	# 	new_log_entry(conn, ("g", "Core", "Step 11/11"))
-	# 	new_log_entry(conn, ("l", "Core", "FINISHED"))
-	# if sf_conn.is_connected:
-		# sf_conn.close()
-	# cursor.close()
-	# conn.close()
-	# print("MySQL connection is closed")
-	# print("Done")
-	pass
+	new_log_entry(conn, ("g", "Core", "FINISHED"))
+	
+	if conn.is_connected():
+		conn.close()
 
-
+	print("MySQL connection is closed")
+	print("Done")
